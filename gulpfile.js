46daf57f14a7ace5706da3a5d7f8dae490ae5927
srldl <siri.uldal@gmail.com>
2015-07-31 11:38:36 +0200

    gulp run browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,22 @@
 var gulp = require('gulp');
-
-/*Read dependencies from package.json and inject each of them*/
+var browserify = require('gulp-browserify');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 
-/*car rename = require('gulp-concat');
-var rename = require('gulp-rename');
-var header = require('gulp-header'); */
-
-
 gulp.task('default', function() {
- return gulp.src('src/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+ return gulp.src(['./src/*.js'])
+    .pipe(browserify())
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('compile-js', ['compile-templates'], function() {
-  return gulp.src('src/*.js')
-  .pipe(concat('controller.js'))
-  .pipe(gulp.dest('./dist/'));
-});
+//gulp.task('compile-js', ['compile-templates'], function() {
+//  return gulp.src('src/*.js')
+//  .pipe(concat('controller.js'))
+//  .pipe(gulp.dest('./dist/'));
+//});
 
 
 
