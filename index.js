0ec0e6b89f76e7c97e545dea4f2729a975c613e4
srldl <siri.uldal@gmail.com>
2015-08-15 10:45:56 +0200

    leaflet-draw - not all working

--- index.js
+++ index.js
@@ -6,27 +6,59 @@ var appMap_test = angular.module('map_test',[]);
 
 // require leaflet.js
 var L = require('leaflet');
+require('leaflet-draw');
 
 // specify the path to the leaflet images folder
-//L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
+L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/'
 
-// initialize the map
-var map = L.map('map', {
-  scrollWheelZoom: false
+// create a map in the "map" div, set the view to a given place and zoom
+var map = L.map('map', {drawControl: true}).setView([78.000, 16.000], 4);
+
+// add an OpenStreetMap tile layer
+L.tileLayer('http://tilestream.data.npolar.no/v2/WorldHax/{z}/{x}/{y}.png', {
+   attribution: 'Norwegian Polar Institute'
+}).addTo(map);
+
+// Initialize the FeatureGroup to store editable layers
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+// Initialize the draw control and pass it the FeatureGroup of editable layers
+var drawControl = new L.Control.Draw({
+   edit: {
+       featureGroup: drawnItems
+   }
+});
+map.addControl(drawControl);
+
+
+map.on('draw:created', function (e) {
+    var type = e.layerType,
+        layer = e.layer;
+        console.log("hei");
+        var res = (layer.toGeoJSON()).geometry.coordinates;
+
+        /*fetch zero and second coordinate pair to get a rectangle */
+        $scope.lat1= res[0][0][0];
+        $scope.lng1= res[0][0][1];
+        $scope.lat2= res[0][2][0];
+        $scope.lng2= res[0][2][1];
+
+
+     console.log(res[0][0][0]);
+    if (type === 'marker') {
+        // Do marker specific actions
+    }
+
+    // Do whatever else you need to. (save to db, add to map etc)
+    drawnItems.addLayer(layer);
 });
 
-// set the position and zoom level of the map
-map.setView([78.00, 16.00], 4);
-
-
-// set the tiles the map will use
-var tiles = 'http://tilestream.data.npolar.no/v2/WorldHax/{z}/{x}/{y}.png';
-
-// create a tileLayer with the tiles, attribution
-var layer = L.tileLayer(tiles, {
-  maxZoom: 14,
-  attribution: 'NPI'
+map.on('draw:edited', function () {
+    // Update db to save latest changes.
+     console.log("hei");
 });
 
-// add the tile layer to the map
-layer.addTo(map);
+map.on('draw:deleted', function () {
+    // Update db to save latest changes.
+});
